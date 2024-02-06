253c8609dd5398c148b016359de4b635868d7958
pastelite <kana.bhong@gmail.com>
2024-02-06 23:30:57 +0700

    finish bottomsheet

--- lib/main.dart
+++ lib/main.dart
@@ -58,7 +58,7 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
-  var _draggableScrollableSheetController = DraggableScrollableController();
+  // var _draggableScrollableSheetController = DraggableScrollableController();
 
   void _incrementCounter() {
     setState(() {
@@ -107,27 +107,141 @@ class _MyHomePageState extends State<MyHomePage> {
             '$_counter',
             style: Theme.of(context).textTheme.headlineMedium,
           ),
-          DraggableScrollableSheet(
-            snapSizes: [0.1, 0.5, 1.0],
-            minChildSize: 0.5,
-            initialChildSize: 0.5,
-            maxChildSize: 1,
-            controller: _draggableScrollableSheetController,
-            builder: (BuildContext context, ScrollController scrollController) {
-              return Container(
-                color: Colors.white,
-                child: ListView.builder(
-                  controller: scrollController,
-                  itemCount: 25,
-                  itemBuilder: (BuildContext context, int index) {
-                    return ListTile(title: Text('Item $index'));
-                  },
-                ),
-              );
-            },
-          ),
+          BottomSheet(
+            child: Container(
+              height: 200,
+              color: Colors.red,
+              child: const Center(
+                child: Text('Hello World'),
+              ),
+            ),
+          )
+          // DraggableScrollableSheet(
+          //   // snapSizes: [0.1, 0.5],
+          //   // snap: true,
+          //   // expand: true,
+          //   minChildSize: 0.5,
+          //   initialChildSize: 0.5,
+          //   maxChildSize: 1,
+          //   // controller: _draggableScrollableSheetController,
+          //   builder: (BuildContext context, ScrollController scrollController) {
+          //     return Container(
+          //       color: Colors.white,
+          //       child: ListView.builder(
+          //         controller: scrollController,
+          //         itemCount: 25,
+          //         itemBuilder: (BuildContext context, int index) {
+          //           return ListTile(title: Text('Item $index'));
+          //         },
+          //       ),
+          //     );
+          //   },
+          // ),
         ],
       ),
+    );
+  }
+}
+
+class BottomSheet extends StatefulWidget {
+  const BottomSheet({
+    super.key,
+    this.minSize = 0.1,
+    this.maxSize = 1,
+    this.child,
+    this.snapSizes = const [0.1, 0.5, 1],
+    this.snapDistance = 0.1,
+  });
+
+  final double minSize;
+  final double maxSize;
+  final Widget? child ;
+  final List<num> snapSizes;
+  final num snapDistance;
+
+  @override
+  State<BottomSheet> createState() => _BottomSheetState();
+}
+
+class _BottomSheetState extends State<BottomSheet> {
+  double _heightFactor = 0.5;
+  bool _isDragging = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (BuildContext context, BoxConstraints constraints) {
+        return Align(
+            alignment: Alignment.bottomCenter,
+            child: AnimatedFractionallySizedBox(
+              duration: Duration(milliseconds: _isDragging ? 0 : 250),
+              curve: Curves.easeInOut,
+              heightFactor: _heightFactor,
+              child: Container(
+                // color: Colors.white,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: const BorderRadius.only(
+                    topLeft: Radius.circular(20),
+                    topRight: Radius.circular(20),
+                  ),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.2),
+                      blurRadius: 10,
+                      spreadRadius: 5,
+                    ),
+                  ],
+                ),
+                child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  GestureDetector(
+                    behavior: HitTestBehavior.opaque,
+                    onVerticalDragUpdate: (details) {
+                      var change = details.delta.dy / constraints.maxHeight;
+                      var factor = (_heightFactor - change);
+                  
+                      setState(() {
+                        _isDragging = true;
+                        _heightFactor =
+                            factor.clamp(widget.minSize, widget.maxSize);
+                      });
+                    },
+                    onVerticalDragEnd: (details) {
+                      // Snap to the nearest value
+                      for (int i = 0; i < widget.snapSizes.length; i++) {
+                        if ((_heightFactor - widget.snapSizes[i]).abs() < widget.snapDistance) {
+                          setState(() {
+                            _heightFactor = widget.snapSizes[i].toDouble();
+                          });
+                        }
+                      }
+
+                      setState(() {
+                        _isDragging = false;
+                      });
+                    },
+                    child: Container(
+                      // margin: const EdgeInsets.symmetric(vertical: 10),
+                      padding: const EdgeInsets.all(10),
+                      child: Align(
+                        alignment: Alignment.center,
+                        child: Container(
+                            height: 6,
+                            width: 50,
+                            decoration: BoxDecoration(
+                              color: Colors.grey,
+                              borderRadius: BorderRadius.circular(10),
+                            )),
+                      ),
+                    ),
+                  ),
+                  widget.child ?? Container()
+                ]),
+              ),
+            ));
+      },
     );
   }
 }
